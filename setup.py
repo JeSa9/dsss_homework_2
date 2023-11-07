463c84740810bb778edd86a0381d8fb7e86b7dd2
JeSa9 <Jessie.Sarris@gmx.de>
2023-11-07 23:10:07 +0100

    Installation script for the configuration of the Python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='math_quiz',
+   version='1.0.0',
+   description='This packages contains a math quiz with some math questions',
+   author='Jessica Sarris',
+   author_email='Jessie.Sarris@gmx.de',
+   packages=[math_quiz],  
+)
