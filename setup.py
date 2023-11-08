d6492adbdd14722aa14ef96321a9ff02938d7fb6
JeSa9 <Jessie.Sarris@gmx.de>
2023-11-08 10:00:07 +0100

    Changed packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    description='This packages contains a math quiz with some math questions',
    author='Jessica Sarris',
    author_email='Jessie.Sarris@gmx.de',
-   packages=[math_quiz],
+   packages=[math_quiz/math_quiz],
    install_requires=[
       "unittest",
       "random",
