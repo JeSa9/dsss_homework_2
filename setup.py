4d8df6dbcf5c802a615df42fa4a3c18f3f7e2249
JeSa9 <Jessie.Sarris@gmx.de>
2023-11-08 10:05:44 +0100

    Changed packages to find_packages()

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from setuptools import find_packages
 from setuptools import setup
 
 setup(
@@ -6,7 +7,7 @@ setup(
    description='This packages contains a math quiz with some math questions',
    author='Jessica Sarris',
    author_email='Jessie.Sarris@gmx.de',
-   packages=[math_quiz/math_quiz],
+   packages=find_packages(),
    install_requires=[
       "unittest",
       "random",
